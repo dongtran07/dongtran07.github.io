33ad45c689c9eaedd4dc9c5c5e8536e9609ecd9c
Tran Manh Dong <dong.tm1212@gmail.com>
2017-01-18 18:37:27 +0700

    fist commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+﻿ $(function(){
+ 	$(window).scroll(function(){
+ 		vitri = $('body').scrollTop();
+ 		console.log('vitri');
+ 		if (vitri > 100){
+ 			$('.navbar').addClass('bienhinh');
+ 		}else{
+ 			$('.navbar').removeClass('bienhinh');
+ 		}
+ 	});
+})
